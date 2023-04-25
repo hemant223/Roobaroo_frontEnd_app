6db3ca662086d06cf7a13808a9872b5513b4edfe
Hemant <hemant@plus91labs.com>
2023-04-25 10:30:19 +0530

    creating project

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { FontFamily } from './src/assets/fonts/FontFamily'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{color:'#000',fontFamily:FontFamily.PopinsExtraBold }}>Appppppppppppppppppppppppppppppppppp</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
