dba3261cb56850691c6b0f53291b1a35c0f54865
Hemant <hemant@plus91labs.com>
2023-04-25 14:33:43 +0530

    merging

---
+++ src/components/componentModals/SuccessModal.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SuccessModal = () => {
+
+
+    
+  return (
+    <View>
+      <Text>SuccessModal</Text>
+    </View>
+  )
+}
+
+export default SuccessModal
+
+const styles = StyleSheet.create({
+
+
+})
